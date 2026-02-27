6b1281bb5b8f433cfcac173dd74a1776ce6cf573
Deepika Musuluri <69344898+DeepikaMusuluri@users.noreply.github.com>
2026-02-26 21:26:31 -0500

    Create LinkedList.java

---
+++ src/linked-list/implementation/LinkedList.java
@@ -0,0 +1,50 @@
+class LinkedList {
+
+    Node head;
+
+    void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+
+        temp.next = newNode;
+    }
+
+    void deleteByValue(int key) {
+        if (head == null) return;
+
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key)
+            temp = temp.next;
+
+        if (temp.next != null)
+            temp.next = temp.next.next;
+    }
+
+    void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
